faa3b06174286c032e9e6023474c16c31a995414
jamesthetab <jsc42@cantab.net>
2015-08-29 21:28:27 +0100

    pythag triples

---
+++ problem9.py
@@ -0,0 +1,11 @@
+a = 1
+
+while a<=997:
+    b = 1
+    while b<= 1000-a-1:
+        c = 1000-a-b
+        if a**2 + b**2 == c**2:
+            print(a,b,c)
+            break
+        b = b+1
+    a= a+1
